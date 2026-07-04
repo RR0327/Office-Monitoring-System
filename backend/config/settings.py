d551c7743bdad25a58ba33885967d531bf6ac7c9
RR0327 <rakibulhassanmiyaji27@gmail.com>
2026-07-04 14:35:23 +0600

    feat: initial project running complete

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,27 +21,42 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ilvx9#+6xiqzzixowo+tcf%nx*m165!!4@558sr7mi$qt3d9(s'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 # Application definition
 
 INSTALLED_APPS = [
+
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    "rest_framework",
+    "corsheaders",
+    "channels",
+
+    # Local Apps
+    "rooms",
+    "devices",
+    "simulator",
+    "alerts",
+    "websocket",
+    "dashboard",
+    "api",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',   # Add this line for CORS headers
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,7 +70,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],  # Add this line to specify the templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,9 +130,60 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "static/"
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+
+    BASE_DIR / "static",
+
+]
+
+# The directory where static files will be collected to when you run `collectstatic`
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Media files (Uploaded files)
+MEDIA_URL = "/media/"
+
+# The directory where uploaded media files will be stored
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+
+    "DEFAULT_PERMISSION_CLASSES": [
+
+        "rest_framework.permissions.AllowAny",
+
+    ],
+
+    "DEFAULT_RENDERER_CLASSES": [
+
+        "rest_framework.renderers.JSONRenderer",
+
+    ],
+
+}
+
+# CORS settings [Later we'll restrict it.]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# Channels settings [Later we'll switch to Redis.]
+ASGI_APPLICATION = "config.asgi.application"
+
+CHANNEL_LAYERS = {
+
+    "default": {
+
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+
+    }
+
+}
+
